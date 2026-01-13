fdb0b1b1d21cf40f56ac20132805f4851db5696e
taka2660 <takahisa.ota@hotmail.com>
2026-01-13 15:41:41 +0900

    修正

--- kadai_010/crud/views.py
+++
@@ -1,9 +0,0 @@
-
-from django.views.generic import DetailView
-from .models import Product
-
-
-#DetailViewクラスを継承したクラスを定義
-class ProductDetailView(DetailView):
-    model = Product
-    template_name = 'product_detail.html'
